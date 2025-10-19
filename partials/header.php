1fa8efdc5ffe2e85a9dd651efac8538f9702ea85
zmalseas <goutoulaschr@gmail.com>
2025-10-19 19:33:22 +0300

    1

--- partials/header.php
+++ partials/header.php
@@ -73,6 +73,11 @@
       <img src="/images/logo.png" alt="Nerally logo" />
       <span class="title">Nerally</span>
     </a>
+    <button class="overlay-close" aria-label="Κλείσιμο μενού">
+      <svg viewBox="0 0 24 24" width="24" height="24">
+        <path fill="currentColor" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+      </svg>
+    </button>
   </div>
 
   <div class="menu-wrap">
